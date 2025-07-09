1cf42cef6f2adef83a610bd21839d2347cbc56d8
vrpst <lucas.verpoest@gmail.com>
2025-07-09 18:04:17 +0100

    word splitting, some hardcoding for testing

--- wordle.sh
+++ wordle.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 target="audio"
+targeta=("a","u","d","i","o")
+
+read guess
 validate_guess() {
-    read guess
-    if [ "${#guess}" -ne 5 ]; then
+    if [ "${#1}" -ne 5 ]; then
         return 1
     else
-        check=$(grep -iwx $guess ./targets)
-        if [ "$check" = $guess ]; then
-            return 0
+        check=$(grep -iwx $1 ./targets)
+        if [ "$check" = $1 ]; then
+            return 0    # success
         else
-            return 1
+            return 1    # not found
         fi
     fi
 }
 
-if validate_guess 1 ; then
-    printf "true\n"
+create_hints() {
+    guessa=(${1:0:1},${1:1:1},${1:2:1},${1:3:1},${1:4:1})
+    printf "$guessa\n"
+    printf "$targeta\n"
+}
+
+if validate_guess $guess 1 ; then
+    if [ $guess = $target ]; then
+        printf "solved!"
+    else
+        create_hints $guess
+        printf "not solved"
+    fi
 else
-    return 1
+    exit
 fi
 
